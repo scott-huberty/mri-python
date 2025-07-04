a21de5de819b4f3af20c7707d3a4b6694fadbef7
Scott Huberty <seh33@uw.edu>
2025-07-03 19:14:35 -0700

    FIX: remove parameter

--- mri/datasets/openneuro.py
+++ mri/datasets/openneuro.py
@@ -1,6 +1,16 @@
 from pathlib import Path
 
 
+def data_path():
+    """Return the default path for OpenNeuro datasets."""
+    # This is a placeholder function to mimic the MNE data path function.
+    # It returns a default path where OpenNeuro datasets can be stored.
+    data_path = Path.home() / "mri_python_data" / "openneuro"
+    if not data_path.exists():
+        data_path.mkdir(parents=True, exist_ok=True)
+    return data_path
+
+
 def check_openneuro_installed():
     """Check if openneuro is installed."""
     try:
@@ -11,19 +21,24 @@ def check_openneuro_installed():
     return openneuro
 
 
-def fetch_pixar_data(
-        dataset="ds000228", subject="pixar001", bids_root=None):
+def fetch_pixar_data(subject="pixar001"):
     """Fetch a dataset from OpenNeuro."""
+    dataset = "ds000228"
+    download_kwargs = dict(include=[f"sub-{subject}"])
+    return fetch_dataset(dataset, download_kwargs=download_kwargs)
+
+
+def fetch_dataset(dataset, download_kwargs=None):
+    """Fetch a dataset from OpenNeuro."""
+    if download_kwargs is None:
+        download_kwargs = {}
+    target_dir = data_path() / dataset
     openneuro = check_openneuro_installed()
-
-    if bids_root is None:
-        bids_root = Path(__file__).parent / dataset
-    if not bids_root.is_dir():
-        bids_root.mkdir()
-
+    if target_dir.exists():
+        return target_dir
     openneuro.download(
         dataset=dataset,
-        target_dir=bids_root,
-        include=[f"sub-{subject}"],
+        target_dir=target_dir,
+        **download_kwargs,
     )
-    return bids_root
+    return target_dir
